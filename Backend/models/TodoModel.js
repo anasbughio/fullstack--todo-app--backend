900f9221b1e28768dce2ff1e79a8397eea5019a4
anasbughio <112558242+anasbughio@users.noreply.github.com>
2024-10-03 13:53:38 +0500

    Add files via upload

---
+++ Backend/models/TodoModel.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+
+const todoSchema = new mongoose.Schema({
+
+    text:{
+        type:String,
+        require:true
+    }
+});
+
+
+module.exports = mongoose.model('practiceTodo',todoSchema);
